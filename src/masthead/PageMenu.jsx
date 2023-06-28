2c166cad04a6f1284ebc7cac8e9318d5e41b9912
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2023-06-28 00:09:14 -0400

    fix menu borders

--- src/masthead/PageMenu.jsx
+++ src/masthead/PageMenu.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Menu } from "semantic-ui-react";
+import { Button, Menu } from "semantic-ui-react";
 import "./Masthead.css";
-// import "../semantic/dist/semantic.min.css";
+import "./Menu.css";
+import "animate.css";
 
 export default class PageMenu extends Component {
   state = { activeItem: "projects" };
@@ -16,13 +17,21 @@ export default class PageMenu extends Component {
         <Menu pointing inverted secondary>
           <Menu.Item href="https://avahajr.github.io" name="about me" />
           <Menu.Item name="projects" active={activeItem === "projects"} />
-          <a
-            href="https://avahajr.github.io/05-24-2023_Resume.pdf"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Menu.Item name="resume" />
-          </a>
+
+          <Menu.Item
+            href="https://avahajr.github.io/classes.html"
+            name="classes"
+          />
+          <Menu.Item position="right">
+            <Button
+              className="animate__animated animate__pulse animate__infinite animate__slower"
+              content="Download resume"
+              as="a"
+              href="https://avahajr.github.io/05-24-2023_Resume.pdf"
+              download
+              color="white"
+            />
+          </Menu.Item>
         </Menu>
       </div>
     );
